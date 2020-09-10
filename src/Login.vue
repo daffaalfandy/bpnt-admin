968bfec296f880207da3fbfac275ce93b07b999b
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-09-10 13:15:13 +0700

    Add login page

---
+++ src/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="h-100 my-5 container">
+    <div class="row h-100 py-5 justify-content-center align-items-center">
+      <div class="card my-5 shadow w-40">
+        <article class="card-body">
+          <div class="row pt-3 pb-4 mx-auto my-auto">
+            <div class="col-3 my-auto mr-0">
+              <img src="./assets/images/icon.png" style="max-width: 4em" />
+            </div>
+            <div class="col-9 my-auto ml-0 pl-0">
+              <p
+                class="h1"
+                style="color: #478419; font-weight: 700; margin-bottom: 0px; padding-bottom: 0px"
+              >BPNT Helper</p>
+            </div>
+          </div>
+          <form>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <i class="fas fa-user-alt"></i>
+                  </div>
+                </div>
+                <input type="text" class="form-control" placeholder="Username" v-model="username" />
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">
+                    <i class="fas fa-key"></i>
+                  </div>
+                </div>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  v-model="password"
+                />
+              </div>
+            </div>
+            <!-- form-group// -->
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </div>
+            <!-- form-group// -->
+          </form>
+        </article>
+      </div>
+      <!-- card.// -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,50 +8,66 @@ import KpmDetail from '../views/KpmDetail'
 import HistoryTransaction from '../views/HistoryTransaction'
 import FinalReport from '../views/FinalReport'
 import IncomeStatement from '../views/IncomeStatement'
+import Login from '../Login'
+import Dash from '../Dash'
 
 window.Vue = Vue
+
+let isAuthenticated = false
+
+
 
 Vue.use(VueRouter)
 
 const routes = [{
+    path: '/login',
+    name: "Login",
+    component: Login
+  },
+  {
     path: '/',
-    name: 'Transaction',
-    component: Transaction
-  },
-  {
-    path: '/main-transaction',
-    name: 'Main Transaction',
-    component: MainTransaction,
-  },
-  {
-    path: '/goods',
-    name: "Goods",
-    component: Goods
-  },
-  {
-    path: '/kpm',
-    name: "KPM",
-    component: Kpm
-  },
-  {
-    path: '/kpm-detail',
-    name: "KPM Detail",
-    component: KpmDetail
-  },
-  {
-    path: '/transaction-history',
-    name: "History Transaction",
-    component: HistoryTransaction
-  },
-  {
-    path: '/final-report',
-    name: "Final Report",
-    component: FinalReport
-  },
-  {
-    path: '/income-statement',
-    name: "Income Statement",
-    component: IncomeStatement
+    component: Dash,
+    children: [{
+        path: '',
+        name: 'Transaction',
+        component: Transaction
+      },
+      {
+        path: 'main-transaction',
+        name: 'Main Transaction',
+        component: MainTransaction,
+      },
+      {
+        path: 'goods',
+        name: "Goods",
+        component: Goods
+      },
+      {
+        path: 'kpm',
+        name: "KPM",
+        component: Kpm
+      },
+      {
+        path: 'kpm-detail',
+        name: "KPM Detail",
+        component: KpmDetail
+      },
+      {
+        path: 'transaction-history',
+        name: "History Transaction",
+        component: HistoryTransaction
+      },
+      {
+        path: 'final-report',
+        name: "Final Report",
+        component: FinalReport
+      },
+      {
+        path: 'income-statement',
+        name: "Income Statement",
+        component: IncomeStatement
+      },
+    ]
   }
 ]
 
@@ -59,6 +75,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !isAuthenticated) next({
+    name: 'Login'
+  })
+  else next()
 })
 
 export default router
